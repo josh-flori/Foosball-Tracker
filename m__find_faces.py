55b5cd9b39c68eacf4e9fe9f1dd3ada30558de52
josh flori <josh.flori@gatewayblend.com>
2019-05-18 15:30:21 -0500

    added face finder

--- m__find_faces.py
+++ m__find_faces.py
@@ -1,62 +1,68 @@
-#!/usr/bin/env python3
-# Copyright 2017 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Trigger PiCamera when face is detected."""
+# download from here... 
+#https://github.com/opencv/opencv/blob/master/samples/dnn/face_detector/opencv_face_detector.pbtxt
+
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 
+# load the input image and construct an input blob for the image
+# by resizing to a fixed 300x300 pixels and then normalizing it
+image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
+	(300, 300), (104.0, 177.0, 123.0))
+    
+# pass the blob through the network and obtain the detections and
+# predictions
+print("[INFO] computing object detections...")
+net.setInput(blob)
+detections = net.forward()
 
 
-# scp pi@192.168.11.2:~/AIY-projects-python/src/examples/vision/faces.jpg /users/josh.flori/desktop
-
-#scp /users/josh.flori/documents/josh-flori/foosball-tracker/test.py  pi@192.168.11.2:~/foos_gods/test.py
-
-#/home/pi/foos_gods/test.py
-
-#//---------------------------------------------------
-#//-----------  FACE_CAMERA_TRIGGER .PY  ------------
-#//---------------------------------------------------
-# change directory so we can import modules normally
-import os
-os.chdir('/home/pi/AIY-projects-python/src/examples/vision')
-from aiy.vision.inference import CameraInference
-from aiy.vision.models import face_detection
-
-from picamera import PiCamera
-
-if not os.path.exists('/home/pi/facial_recognition/'):
-    os.makedirs('/home/pi/facial_recognition/')
-
-def main():
-    print("1")
-    # with PiCamera() as camera:
-    #     # Configure camera
-    #     camera.resolution = (1640, 922)  # Full Frame, 16:9 (Camera v2)
-    #     camera.start_preview()
-    #
-    #     # Do inference on VisionBonnet
-    #     i=1
-    #     with CameraInference(face_detection.model()) as inference:
-    #         for result in inference.run():
-    #             if len(face_detection.get_faces(result)) >= 1:
-    #                 camera.capture('faces'+str(i)+'.jpg')
-    #                 i+=1
-    #
-    #     # Stop preview
-    #     camera.stop_preview()
-
-if __name__ == '__main__':
-    main()
-
-
-
+# loop over the detections
+for i in range(0, detections.shape[2]):
+	# extract the confidence (i.e., probability) associated with the
+	# prediction
+	confidence = detections[0, 0, i, 2]
+ 
+	# filter out weak detections by ensuring the `confidence` is
+	# greater than the minimum confidence
+	if confidence > args["confidence"]:
+		# compute the (x, y)-coordinates of the bounding box for the
+		# object
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		print(box)
+		(startX, startY, endX, endY) = box.astype("int")
+ 
+		# draw the bounding box of the face along with the associated
+		# probability
+		text = "{:.2f}%".format(confidence * 100)
+		y = startY - 10 if startY - 10 > 10 else startY + 10
+		cv2.rectangle(image, (startX, startY), (endX, endY),
+			(0, 0, 255), 2)
+		cv2.putText(image, text, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+		roi = image[startY:endY, startX:endX]
+		cv2.imwrite("roi.jpg", roi)
+ 
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)    

0adbd075296ab4423476f6f911e40f0495927792
josh flori <josh.flori@gatewayblend.com>
2019-05-19 10:01:02 -0500

    updating network

--- m__find_faces.py
+++ m__find_faces.py
@@ -1,4 +1,4 @@
-# /users/josh.flori/desktop/test/bin/python3 /users/josh.flori/documents/josh-flori/foosball-tracker/m__find_faces.py -p='/users/josh.flori/desktop/opencv_face_detector.pbtxt.txt' -m='/users/josh.flori/desktop/res10_300x300_ssd_iter_140000.caffemodel' -o='/users/josh.flori/desktop/' -v='/users/josh.flori/a.mp4'
+# /users/josh.flori/desktop/test/bin/python3 /users/josh.flori/documents/josh-flori/foosball-tracker/m__find_faces.py -p='/users/josh.flori/desktop/opencv_face_detector.pbtxt.txt' -m='/users/josh.flori/desktop/res10_300x300_ssd_iter_140000.caffemodel' -o='/users/josh.flori/desktop/' -v='/users/josh.flori/a.mp4' -n=2
 
 
 
@@ -22,6 +22,8 @@ ap.add_argument("-o", "--output_dir", required=True,type=str,
 	help="output path for faces")
 ap.add_argument("-v", "--video_file", required=True,type=str,
 	help="path to input video file")    
+ap.add_argument("-n", "--num_faces", required=True,type=int,
+	help="number of people in the shot")
     
 args = vars(ap.parse_args())
 
@@ -42,32 +44,38 @@ def output_faces(frame,frame_num):
     #print("[INFO] computing object detections...")
     net.setInput(blob)
     detections = net.forward()
-
-    # loop over the detections
-    for face in range(0, detections.shape[2]):
-    	# extract the confidence (i.e., probability) associated with the
-    	# prediction
-    	confidence = detections[0, 0, face, 2]
+    
+    # count the number of faces.
+    confidences = [detections[0, 0, i, 2] for i in range(0, detections.shape[2]) if detections[0, 0, i, 2] > args["confidence"]]
+    
+    # there are many artifact faces detected. sometimes there will be more or less than there should be. in that case, we would be unable to assume proper Y labeling in train_network.py. restricting to the proper number of faces keeps us in line with auto-Y labeling.
+    if len(confidences)==args["num_faces"]:
+        
+        # loop over the detections
+        for face in range(0, detections.shape[2]):
+        	# extract the confidence (i.e., probability) associated with the
+        	# prediction
+        	confidence = detections[0, 0, face, 2]
  
-    	# filter out weak detections by ensuring the `confidence` is
-    	# greater than the minimum confidence
-    	if confidence > args["confidence"]:
-    		# compute the (x, y)-coordinates of the bounding box for the
-    		# object
-    		box = detections[0, 0, face, 3:7] * np.array([w, h, w, h])
-    		print(box)
-    		(startX, startY, endX, endY) = box.astype("int")
+        	# filter out weak detections by ensuring the `confidence` is
+        	# greater than the minimum confidence
+        	if confidence > args["confidence"]:
+        		# compute the (x, y)-coordinates of the bounding box for the
+        		# object
+        		box = detections[0, 0, face, 3:7] * np.array([w, h, w, h])
+        		
+        		(startX, startY, endX, endY) = box.astype("int")
  
-    		# draw the bounding box of the face along with the associated
-    		# probability
-    		text = "{:.2f}%".format(confidence * 100)
-    		y = startY - 10 if startY - 10 > 10 else startY + 10
-    		cv2.rectangle(frame, (startX, startY), (endX, endY),
-    			(0, 0, 255), 2)
-    		cv2.putText(frame, text, (startX, y),
-    			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-    		cropped = frame[startY:endY, startX:endX]
-    		cv2.imwrite(args["output_dir"]+"frame_"+str(frame_num)+"_person_"+str(face)+".jpg", cropped)
+        		# draw the bounding box of the face along with the associated
+        		# probability
+        		text = "{:.2f}%".format(confidence * 100)
+        		y = startY - 10 if startY - 10 > 10 else startY + 10
+        		cv2.rectangle(frame, (startX, startY), (endX, endY),
+        			(0, 0, 255), 2)
+        		cv2.putText(frame, text, (startX, y),
+        			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        		cropped = frame[startY:endY, startX:endX]
+        		cv2.imwrite(args["output_dir"]+"frame_"+str(frame_num)+"_person_"+str(face)+".jpg", cropped)
  
 
 
--- m__train_network.py
+++ m__train_network.py
@@ -1,45 +1,52 @@
 # THE PURPOSE OF THIS SCRIPT IS TO TRAIN A NETWORK TO RECOGNIZE OUR FACES
+# /users/josh.flori/desktop/test/bin/python3 /users/josh.flori/documents/josh-flori/foosball-tracker/m__train_network.py -i='/users/josh.flori/desktop/' -f 0 1
+
 from __future__ import absolute_import, division, print_function
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import imageio
+import cv2
 import pandas as pd
 import argparse
+import matplotlib.pyplot as plt
 
  
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-ap.add_argument("-o", "--output_dir", required=True,type=str,
-	help="output path for faces")
-ap.add_argument("-v", "--video_file", required=True,type=str,
-	help="path to input video file")    
+ap.add_argument("-i", "--path_to_images", required=True,type=str,
+	help="path to input images")
+ap.add_argument('-f', '--faces_to_text', nargs='*', default=[],help="a text list of the names of the faces corresponding to the 0th:nth faces represented in the images to be loaded") # # find_faces.py outputs naming structure like: "frame_0_person_0", so if person 0 was "bob" then your list would be ["bob"] and so on for all faces)
+ap.add_argument('-w', '--path_to_model',type=str, default='',help='if first time training model, this should be the path to where you want to save you model, else, path to where you already saved your model.')
+ap.add_argument('-f', '--first_time_training_model',type=str, default='',help="should be either (y,n), determines whether we load existing model weights or not.")
+
+    
+
+args = vars(ap.parse_args())
+
+
+def getfiles(dirpath):
+    images = [s for s in os.listdir(dirpath)
+         if os.path.isfile(os.path.join(dirpath, s)) and "jpg" in s]
+    images.sort(key=lambda s: os.path.getmtime(os.path.join(dirpath, s)))
+    return images
 
 
 
-print(tf.__version__)
 
-##################
-#      PATH      #
-##################
-path = '/users/josh.flori/desktop/images/'
-m = len([i for i in os.listdir(path) if 'jpg' in i])
+#####################
+#    LOAD IAMGES    #
+#####################
+images=getfiles(args["path_to_images"])
 
-##################
-#    load x/y    #
-##################
-X = np.array([imageio.imread(path + str(i) + '.jpg') for i in range(1, m + 1)])
-Y = np.array(pd.read_csv('/users/josh.flori/desktop/Y.csv')['Y'].values.tolist())
-X.shape
-Y.shape
+    
+X = np.array([cv2.resize(cv2.imread(args["path_to_images"]+image),(50,50)) for image in images])
+Y = np.array([args["faces_to_text"][i] for i in range(len(args["faces_to_text"]))]*int((len(images)/len(args["faces_to_text"]))))
+
+print(X.shape)
+print(Y.shape)
+print(len(images))
 
 ##########################
 #    STANDARDIZE DATA    #
@@ -49,17 +56,19 @@ X = X / 255
 ##########################
 #   ASSERTION CHECKING   #
 ##########################
-assert (X.shape[0] == m)
-assert (Y.shape[0] == m)
+assert (X.shape[0] == len(images))
+assert (Y.shape[0] == len(images))
+print(X.shape)
+print(Y.shape)
 
 ##########################
 #     VISUALIZE DATA     #
 ##########################
-for i in range(10):
-    print(Y[i])
-    plt.figure()
-    plt.imshow(X[i])
-    plt.show()
+# for i in range(10):
+#     print(Y[i])
+#     plt.figure()
+#     plt.imshow(X[i])
+#     plt.show()
 
 #####################
 #     RANDOMIZE     #
@@ -78,25 +87,61 @@ train_Y, dev_Y, test_Y = np.split(Y, [int(.8 * X.shape[0]), int(.9 * X.shape[0])
 ########################
 #     DEFINE MODEL     #
 ########################
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(35, 20, 3)),
-    keras.layers.Dense(50, activation=tf.nn.sigmoid),
-    keras.layers.Dense(50, activation=tf.nn.sigmoid),
-    keras.layers.Dense(50, activation=tf.nn.sigmoid),
-    keras.layers.Dense(2, activation=tf.nn.softmax)
-])
 
-########################
-#     COMPILE MODEL    #
-########################
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+if args["first_time_training_model"].lower()=="y":
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(X[0].shape[0], X[0].shape[1], 3)),
+        keras.layers.Dense(50, activation=tf.nn.sigmoid),
+        keras.layers.Dense(50, activation=tf.nn.sigmoid),
+        keras.layers.Dense(50, activation=tf.nn.sigmoid),
+        keras.layers.Dense(len(args['faces_to_text']), activation=tf.nn.softmax)
+    ])
+
+    ########################
+    #     COMPILE MODEL    #
+    ########################
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+else:
+    model = tf.keras.models.load_model(args["path_to_model"])
+    
+
 
 #####################
 #      FIT MODEL    #
 #####################
-model.fit(train_X, train_Y, epochs=100, verbose=2, validation_data=(dev_X, dev_Y))
+history = model.fit(train_X, train_Y, epochs=500, verbose=2, validation_data=(dev_X, dev_Y))
+history_dict = history.history
+train_acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+train_loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+
+def plot_diagnostics(train_acc,val_acc,train_loss,val_loss,which="accuracy"):
+    epochs = range(1, len(train_acc) + 1)
+    if which=="loss":
+        plt.plot(epochs, train_loss, 'bo', label='Training loss')
+        plt.plot(epochs, val_loss, 'b', label='Validation loss')
+        plt.title('Training and validation loss')
+        plt.ylabel('Loss')
+    else:
+        plt.plot(epochs, train_acc, 'bo', label='Training accuracy')
+        plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+        plt.title('Training and validation accuracy')
+        plt.ylabel('Accuracy')
+        
+    plt.xlabel('Epochs')
+    plt.legend()
+    plt.show()
+
+
+
+plot_diagnostics(train_acc,val_acc,train_loss,val_loss,"loss")
+plot_diagnostics(train_acc,val_acc,train_loss,val_loss,"accuracy")
+
+
 
 ################################
 #     EVALUATE ON TEST DATA    #
@@ -109,3 +154,12 @@ model.evaluate(test_X, test_Y)
 predictions = model.predict(test_X)
 predictions.shape
 np.argmax(predictions[0])
+
+
+save_model=input("would you like to save this model (weights, state) and overwrite any previously saved weights? (y/n)")
+if save_model.lower()=="y":
+    model.save(args["path_to_model"])
+
+
+
+

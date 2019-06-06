8ddf862f647d661ad59bdd635746b0f539f8a2eb
josh flori <josh.flori@gatewayblend.com>
2019-06-06 14:31:49 -0500

    Update getting in to the linux thing.py

--- getting in to the linux thing.py
+++ getting in to the linux thing.py
@@ -1,10 +1,39 @@
 # https://aiyprojects.withgoogle.com/vision-v1/
+
+
+# RIGHT NOW TO TRAIN MODEL..... DO
+cd /users/josh.flori/documents/josh-flori/tensorflow-for-poets-2
+# confirm data labels
+ls tf_files/training_data
+IMAGE_SIZE=160
+ARCHITECTURE="mobilenet_0.50_${IMAGE_SIZE}"
+
+python -m scripts.retrain \
+  --bottleneck_dir=tf_files/bottlenecks \
+  --how_many_training_steps=100 \
+  --model_dir=tf_files/models/"${ARCHITECTURE}" \
+  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+  --output_graph=tf_files/retrained_graph.pb \
+  --output_labels=tf_files/retrained_labels.txt \
+  --architecture="${ARCHITECTURE}" \
+  --image_dir=tf_files/training_data
+
+
+
+
+python -m scripts.label_image \
+    --graph=tf_files/retrained_graph.pb  \
+    --image=/users/josh.flori/desktop/frame_263.jpg
+
+
+
+
 # made a file executable in linux by chmod
 chmod +x bonnet_model_compiler.par 
 
 # to get a new docker instance from folder.... using the version of linux with python preinstalled
 docker run \
-  --name aaqaaaaaaaa \
+  --name aaqaa3aa \
   -e HOST_IP=$(ifconfig en1 | awk '/ *inet /{print $2}') \
   -v /Users/josh.flori/desktop/send_to_linux:/src \
   -t -i \
@@ -16,7 +45,7 @@ ls /src
 
 
 /src/a/bonnet_model_compiler.par \
-    --frozen_graph_path=/src/a/mobilenet_v1_160res_0.5_imagenet.pb \
+    --frozen_graph_path=/src/a/frozen_graph.pb \
     --output_graph_path=/cat_detector.binaryproto \
     --input_tensor_name='input' \
     --output_tensor_names='MobilenetV1/Predictions/Softmax' \
